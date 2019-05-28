93c5fbd4958ffe44ba06a52d8ec09aaf0688a6d2
Steven G. Harms <steven.harms@flatironschool.com>
2019-05-28 15:17:18 -0400

    Use @babel/{core,preset-env}

--- test/helpers.js
+++ test/helpers.js
@@ -3,13 +3,13 @@ global.expect = chai.expect
 const fs = require('file-system')
 const jsdom = require('mocha-jsdom')
 const path = require('path')
-const babel = require('babel-core');
+const babel = require("@babel/core");
 
 const html = fs.readFileSync(path.resolve(__dirname, '..', 'index.html'), 'utf-8')
 
 const babelResult = babel.transformFileSync(
   path.resolve(__dirname, '..', 'index.js'), {
-    presets: ['env']
+    presets: ["@babel/preset-env"]
   }
 );
 
